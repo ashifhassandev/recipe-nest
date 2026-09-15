1aa030c898ae6e7c10f6138e699f828621d8b185
Ashif Hassan <ashifhassan1996@gmail.com>
2026-09-15 19:21:49 +0530

    style: format code with Prettier

--- middlewares/jwtMiddleware.js
+++ middlewares/jwtMiddleware.js
@@ -5,42 +5,44 @@ const httpStatusCode = require("../utils/httpStatusCode");
 
 // Middleware to authenticate JWT Tokens
 const authenticateJWT = (req, res, next) => {
-    const token = req.cookies.authToken || req.header("Authorization")?.replace("Bearer ", "");
+  const token =
+    req.cookies.authToken ||
+    req.header("Authorization")?.replace("Bearer ", "");
 
-    if (!token) {
+  if (!token) {
+    return showFlashMessages({
+      req,
+      res,
+      message: "Access denied. Please log in.",
+      status: httpStatusCode.UNAUTHORIZED,
+      redirectUrl: "/users/login",
+    });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      if (err.name === "TokenExpiredError") {
         return showFlashMessages({
-            req,
-            res,
-            message: "Access denied. Please log in.",
-            status: httpStatusCode.UNAUTHORIZED,
-            redirectUrl: "/users/login",
+          req,
+          res,
+          message: "Session has expired. Please log in again.",
+          status: httpStatusCode.UNAUTHORIZED,
+          redirectUrl: "/users/login",
         });
+      }
+
+      return showFlashMessages({
+        req,
+        res,
+        message: "Invalid token. Please log in again.",
+        status: httpStatusCode.UNAUTHORIZED,
+        redirectUrl: "/users/login",
+      });
     }
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if (err) {
-            if (err.name === "TokenExpiredError") {
-                return showFlashMessages({
-                    req,
-                    res,
-                    message: "Session has expired. Please log in again.",
-                    status: httpStatusCode.UNAUTHORIZED,
-                    redirectUrl: "/users/login",
-                });
-            }
-
-            return showFlashMessages({
-                req,
-                res,
-                message: "Invalid token. Please log in again.",
-                status: httpStatusCode.UNAUTHORIZED,
-                redirectUrl: "/users/login",
-            });
-        }
-
-        req.user = decoded;
-        next();
-    });    
+    req.user = decoded;
+    next();
+  });
 };
 
 module.exports = authenticateJWT;
