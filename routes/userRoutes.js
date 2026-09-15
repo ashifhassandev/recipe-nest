4fcc0bcbde3f7aa0f9264025e609c93a35a38948
Ashif Hassan <ashifhassan1996@gmail.com>
2026-09-15 19:32:15 +0530

    style: format code with Prettier

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,41 +5,52 @@ const userController = require("../controllers/userController");
 const authenticateJWT = require("../middlewares/jwtMiddleware");
 const upload = require("../config/multerConfig");
 
-router.route("/login")
-    .get(userController.getUserLogin)
-    .post(userController.userLogin);
+router
+  .route("/login")
+  .get(userController.getUserLogin)
+  .post(userController.userLogin);
 
-router.route("/signup")
-    .get(userController.getUserSignup)
-    .post(userController.userSignup);
+router
+  .route("/signup")
+  .get(userController.getUserSignup)
+  .post(userController.userSignup);
 
-router.get('/logout', userController.userLogout);
+router.get("/logout", userController.userLogout);
 
 router.get("/user-profile", authenticateJWT, userController.getUserProfile);
 
-router.route("/user-profile/edit/:id")
-    .get(authenticateJWT, userController.getEditUserProfile)
-    .put(authenticateJWT, userController.editUserProfile);
+router
+  .route("/user-profile/edit/:id")
+  .get(authenticateJWT, userController.getEditUserProfile)
+  .put(authenticateJWT, userController.editUserProfile);
 
-router.get('/favourite-recipes', authenticateJWT, userController.getFavouriteRecipes);
+router.get(
+  "/favourite-recipes",
+  authenticateJWT,
+  userController.getFavouriteRecipes,
+);
 
 router.post("/add-favourites", userController.addFavouriteRecipes);
 
-router.route("/add-category")
-    .get(authenticateJWT, userController.getAddCategory)
-    .post(authenticateJWT, upload, userController.addCategory);
+router
+  .route("/add-category")
+  .get(authenticateJWT, userController.getAddCategory)
+  .post(authenticateJWT, upload, userController.addCategory);
 
-router.route("/edit-category/:id")
-    .get(authenticateJWT, userController.getEditCategory)
-    .put(authenticateJWT, upload, userController.editCategory);
+router
+  .route("/edit-category/:id")
+  .get(authenticateJWT, userController.getEditCategory)
+  .put(authenticateJWT, upload, userController.editCategory);
 
-router.route("/add-recipe")
-    .get(authenticateJWT, userController.getAddRecipe)
-    .post(authenticateJWT, upload, userController.addRecipe);
+router
+  .route("/add-recipe")
+  .get(authenticateJWT, userController.getAddRecipe)
+  .post(authenticateJWT, upload, userController.addRecipe);
 
-router.route("/edit-recipe/:id")
-    .get(authenticateJWT, userController.getEditRecipe)
-    .put(authenticateJWT, upload, userController.editRecipe);
+router
+  .route("/edit-recipe/:id")
+  .get(authenticateJWT, userController.getEditRecipe)
+  .put(authenticateJWT, upload, userController.editRecipe);
 
 router.post("/contact/send-email", userController.processSendEmail);
 
