c06d48e179d4bdba3134b894361a99febdcf0eae
Ashif Hassan <ashifhassan1996@gmail.com>
2026-09-16 10:18:02 +0530

    style: format code with Prettier

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(methodOverride("_method"));
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://recipe-nest-b24f.onrender.com",
     credentials: true,
   }),
 );
